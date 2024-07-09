bc8376b63395983ae43dfe2a3fbc903e42064622
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-09 16:11:48 +0200

    Initial commit

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+
+    <span class="rock-salt-regular text-body-tertiary p-1 m-1">
+        <i class="fas fa-yin-yang"></i> AP.
+    </span>
+
+
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('main')
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="{{ Vite::asset('resources/img/pipistrello.jpg')}}" alt="">
+                <ul>
+                    <li><a href="">prova list item e link</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endsection
