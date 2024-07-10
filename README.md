003edd6e69ea2195c18e61e44ed6ee5218a50563
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-10 16:45:51 +0200

    [UPD] Creazione seeder per la tabella trains

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Trains
 
+## Prima parte
+
 -   Creiamo una tabella trains e relativa Migration
 -   Ogni treno dovrà avere:
     -   Azienda
@@ -17,3 +19,10 @@
 Inserite inizialmente i dati tramite PhpMyAdmin.
 Create Model relativo ed un Controller per mostrare nella home page tutti i treni che sono in partenza dalla data odierna.
 ```
+
+## Seconda parte
+
+-   Fate una revisione del vostro codice con quanto visto questa mattina a lezione;
+-   Create un seeder per popolare la vostra tabella con almeno 50 treni utilizzando Faker.
+-   Bonus:
+    -   Utilizzando lo stesso layout, provate a creare una pagina di dettaglio del treno, raggiungibile cliccando sulla card nell'elenco dei treni in partenza.
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $table->string('azienda', 100);
+        // $table->string('stazione_partenza', 100);
+        // $table->string('stazione_arrivo', 100);
+        // $table->date('partenza_data');
+        // $table->time('partenza_orario');
+        // $table->date('arrivo_data');
+        // $table->time('arrivo_orario');
+        // $table->integer('codice_treno')->unique()->unsigned()->after('id');
+        // $table->integer('numero_carrozze')->unsigned();
+        // $table->boolean('is_in_orario')->default(true);
+        // $table->boolean('is_cancellato')->default(false);
+    }
+}
